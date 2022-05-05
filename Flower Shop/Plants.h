d528495cc632d2ea7b8d0a22880eaa97405bbdba
Alexander Peev <103655534+sashimiP@users.noreply.github.com>
2022-05-06 00:10:55 +0300

    Update Plants.h

--- Flower Shop/Plants.h
+++ Flower Shop/Plants.h
@@ -1,1 +1,24 @@
+#include<vector>
+#include<string>
+#include<windows.h>
+#include<algorithm>
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
 
+class Plants
+{
+protected:
+	string name{};
+	float price{};
+	unsigned int quantity{};
+	string type;
+public:
+	void virtual EnterData() = 0;
+	void virtual Display() const = 0;
+	void virtual SetType(string type) = 0;
+	float virtual GetPrice() const = 0;
+	string virtual GetColour() const { return ""; }
+};
