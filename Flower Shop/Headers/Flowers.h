b6859e34b7439ca238e545ef3005bfe86793a5f7
Alexander Peev <103655534+sashimiP@users.noreply.github.com>
2022-05-07 00:46:21 +0300

    Update Flowers.h

--- Flower Shop/Headers/Flowers.h
+++ Flower Shop/Headers/Flowers.h
@@ -1,16 +1,15 @@
 #pragma once
 #include"Plants.h"
-class Trees: public Plants
+class Flowers: public Plants
 {
 private:
-	unsigned int height{};
-	string treeType{};
-
+	string colour{};
 public:
 	void EnterData();
 	void Display() const;
 	void SetType(string type);
 	float GetPrice() const;
+	string GetColour() const;
 	string StringToTitle(string name) const;
-	
+
 };
