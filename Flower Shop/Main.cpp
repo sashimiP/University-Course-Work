dc46d1fb5b4f1a14706b9b8dedc33c6e2d8cea2c
Alexander Peev <103655534+sashimiP@users.noreply.github.com>
2022-05-06 14:03:17 +0300

    Update Main.cpp

--- Flower Shop/Main.cpp
+++ Flower Shop/Main.cpp
@@ -2,29 +2,12 @@
 
 int main()
 {
-	unsigned short operation;
+	
 	vector<Plants*>plants;
 	vector<Plants*>redFlowers;
-
-	cout << "\t**MENU**\n";
-	cout << "\t-1- Add a new plant.\n";
-	cout << "\t-2- Display all the available plants.\n";
-	cout << "\t-3- Display most expensive plant.\n";
-	cout << "\t-4- Create an array of all the red flowers and display it.\n";
-	do {
-		cout << "Choose an operation or press 0 to exit.\n";
-		cin >> operation;
-		switch (operation) {
-		case 1: addNewPlant(plants); break;
-		case 2: displayAllPlants(plants); break;
-		case 3: displayMostExpPlat(plants); break;
-		case 4: RedFlowers(plants, redFlowers); break;
-		case 0: break;
-		default:cout << "Wrong operation\n"; break;
-		}
-
-	} while (operation != 0);
 	
+	DisplayMenu();
+	MenuOptions(plants, redFlowers);
 
 	return 0;
 }
